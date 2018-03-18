6f58e4da74c7dda6abbfac41cbb993499732e48d
Danny Danger <dannywithachance@gmail.com>
2018-03-18 14:38:48 -0700

    changing branches

--- js/app.js
+++ js/app.js
@@ -2,9 +2,22 @@
 
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+document.getElementById("day").innerHTML = n;
+
 var table = document.getElementById("schedule");
 
-function Plant (name, type, filepath, freqOfWatering) {
+function Plant (name, type, filePath, freqOfWatering) {
     this.name = name;
     this.type = type;
     this.filePath = filePath;
