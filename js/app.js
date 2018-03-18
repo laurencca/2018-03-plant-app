4940a469834b188093e9d97b238bfeff2c2908ac
Danny Danger <dannywithachance@gmail.com>
2018-03-18 11:27:20 -0700

    completed dynamimc table

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function Plant (name, type, filepath, freqOfWatering) {
+function Plant (name, type, filePath, freqOfWatering) {
     this.name = name;
     this.type = type;
-    this.filepath = filepath;
+    this.filePath = filePath;
     this.freqOfWatering = freqOfWatering;
 }
 
@@ -12,75 +12,63 @@ const schedule = {
     selectedPlants: [],
     start: function () {
         schedule.plants.push(
-            new Plant('tomato', 'veg', '', 2),
-            new Plant('lettuce', 'veg', '', 2),
-            new Plant('peas', 'veg', '', 4),
-            new Plant('corn', 'veg', '', 7),
-            new Plant('squash', 'veg', '', 1),
-            new Plant('iris', 'flower', '', 3),
-            new Plant('rose', 'flower', '', 1),
-            new Plant('daylily', 'flower', '', 3),
-            new Plant('violet', 'flower', '', 2),
-            new Plant('peony', 'flower', '', 14),                 
+            new Plant('Tomato', 'veg', '', 2),
+            new Plant('Lettuce', 'veg', '', 2),
+            new Plant('Peas', 'veg', '', 4),
+            new Plant('Corn', 'veg', '', 7),
+            new Plant('Squash', 'veg', '', 1),
+            new Plant('Iris', 'flower', '', 3),
+            new Plant('Rose', 'flower', '', 1),
+            new Plant('Daylily', 'flower', '', 3),
+            new Plant('Violet', 'flower', '', 2),
+            new Plant('Peony', 'flower', '', 14),                 
         )
         makeTable();
     }
 }
 
-// var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var table = document.getElementById("schedule");
 
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+function dayHeader() {
+    var weekRow = document.createElement("tr");
+    console.log(table);
+    table.appendChild(weekRow);
+    var cellSpace = document.createElement("td");
+    cellSpace.textContent = "";
+    weekRow.appendChild(cellSpace);
+    for (var weekIndex = 0; weekIndex < daysOfWeek.length; weekIndex++) {
+        var cellDay = document.createElement("td");
+        cellDay.textContent = daysOfWeek[weekIndex];
+        weekRow.appendChild(cellDay);
+    }
+}
 
-function buildGarden() {
-    var ownership = document.getElementsByClassName("plants");
-    var txt = "";
-    for (var index = 0; index < ownership.length; index++) {
-        if (ownership[index].checked) {
-           
+function makeTable() {  
+    table.textContent = "";
+    dayHeader();
+    for (var plantIndex = 0; plantIndex < schedule.plants.length; plantIndex++) {
+        console.log("in loop");
+        var plants = schedule.plants[plantIndex];
+        var plantsRow = document.createElement("tr");
+        var cell = document.createElement("td");
+        cell.textContent = plants.name;
+        plantsRow.appendChild(cell);
+        table.appendChild(plantsRow);
         }
     }
-}
-//     var html = "";
-//     for (var index = 0; index < selectedPlants.length; index++){
-//     html +="<tr>"+
-//             "<td>"+ (index+1) + "</td>"+
-//             "<td>"+ data[index].name + "</td>"+
-//             "<td>"+ data[index].number + "</td>"+
-//             "<td>"+ data[index].city + "</td>"+
-//             "<td>"+ data[index].hobby + "</td>"+
-//             "<td>"+ data[index].birthdate + "</td>"+"<td><button data-arrayIndex='"+ i +"' onclick='editData(this)'>Edit</button><button data-arrayIndex='"+ i +"' onclick='deleteData()'>Delete</button></td>"+"</tr>";
-// }
-
-
-// function dayHeader() {
-//     var rowDay = document.createElement("th");
-//     table.appendChild(rowDay);
-//     var cellLocation = document.createElement("td");
-//     cellLocation.textContent = "";
-//     rowDay.appendChild(cellLocation);
-
-//     for (var dayIndex = 0; dayIndex < daysOfWeek.length; dayIndex++) {
-//         var cell = document.createElement("td");
-//         cell.textContent = daysOfWeek[dayIndex];
-//         rowDay.appendChild(cell);
+//     var table = document.createElement("table");
+//     var plants = "";   
+//     var rows = 10;
+//     var columns = 7;
+//     for (var plantRow = 0; plantRow < rows; plantRow++) {
+//         plants += "<tr>";
+//         for (var dayColumn = 1; dayColumn < columns; dayColumn++) {
+//             plants += "<td>" + dayColumn + "</td>";
 //         }
-// }
-
-function makeTable() { 
-    debugger;
-    // var table = document.getElementById("schedule");
-    // table.innerHTML = "";
-    var plantRow = document.getElementsByTagName("tr");
-    console.log(schedule.plants);
-    for (var plantIndex = 0; plantIndex < schedule.plants.length; plantIndex++) {
-        var plant = schedule.plants[plantIndex];
-        console.log(plant);
-        var cell = document.createElement("td");
-        cell.textContent = plant.name;
-        plantRow[1].appendChild(cell);
-
-    }
-}
+//         plants += "</tr>"
+//     }        
+//     console.log(plants);
 
 window.addEventListener('load', schedule.start)
-// makeTable();
